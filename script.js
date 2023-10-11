250e5143964d4d3e07adffd6c9b248a9657a3dc0
PasTof <125820486+PasTof@users.noreply.github.com>
2023-10-11 19:34:23 +0200

    final change

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 let fields = [
   null,
-  'circle',
-  'cross',
+  null,
+  null,
   null,
   null,
   null,
@@ -10,29 +10,35 @@ let fields = [
   null,
   null,
 ];
-
+const WINNING_COMBINATIONS = [
+  [0, 1, 2], [3, 4, 5], [6, 7, 8], // horizontal
+  [0, 3, 6], [1, 4, 7], [2, 5, 8], // vertical
+  [0, 4, 8], [2, 4, 6], // diagonal
+];
+let currentPlayer = 'circle';
 function init() {
   render();
 }
 
+
 function render() {
   const contentDiv = document.getElementById('content');
 
-  // Generate table HTML
+
   let tableHtml = '<table>';
   for (let i = 0; i < 3; i++) {
-      tableHtml += '<tr>';
-      for (let j = 0; j < 3; j++) {
-          const index = i * 3 + j;
-          let symbol = '';
-          if (fields[index] === 'circle') {
-              symbol = generateCircleSVG();
-          } else if (fields[index] === 'cross') {
-              symbol = generateCrossSVG();
-          }
-          tableHtml += `<td>${symbol}</td>`;
+    tableHtml += '<tr>';
+    for (let j = 0; j < 3; j++) {
+      const index = i * 3 + j;
+      let symbol = '';
+      if (fields[index] === 'circle') {
+        symbol = generateCircleSVG();
+      } else if (fields[index] === 'cross') {
+        symbol = generateCrossSVG();
       }
-      tableHtml += '</tr>';
+      tableHtml += `<td onclick="handleClick(this, ${index})">${symbol}</td>`;
+    }
+    tableHtml += '</tr>';
   }
   tableHtml += '</table>';
 
@@ -40,7 +46,47 @@ function render() {
   contentDiv.innerHTML = tableHtml;
 }
 
+function restartGame() {
+  fields = [
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+    null,
+  ];
+  render();
+}
 
+function handleClick(cell, index) {
+  if (fields[index] === null) {
+    fields[index] = currentPlayer;
+    cell.innerHTML = currentPlayer === 'circle' ? generateCircleSVG() : generateCrossSVG();
+    cell.onclick = null;
+    currentPlayer = currentPlayer === 'circle' ? 'cross' : 'circle';
+    if (isGameFinished()) {
+      const winCombination = getWinningCombination();
+      drawWinningLine(winCombination);
+    }
+  }
+}
+function isGameFinished() {
+  return fields.every((field) => field !== null) || getWinningCombination() !== null;
+}
+
+function getWinningCombination() {
+  for (let i = 0; i < WINNING_COMBINATIONS.length; i++) {
+ 
+    const [a, b, c] = WINNING_COMBINATIONS[i]; 
+    if (fields[a] === fields[b] && fields[b] === fields[c] && fields[a] !== null) {
+      return WINNING_COMBINATIONS[i];
+    }
+  }
+  return null;
+}
 function generateCircleSVG() {
   const color = '#00B0EF';
   const width = 70;
@@ -55,6 +101,7 @@ function generateCircleSVG() {
 
 
 function generateCrossSVG() {
+
   const color = '#FFC000';
   const width = 70;
   const height = 70;
@@ -73,6 +120,32 @@ function generateCrossSVG() {
       </line>
     </svg>
   `;
-
   return svgHtml;
+}
+function drawWinningLine(combination) {
+  const lineColor = '#ffffff';
+  const lineWidth = 5;
+
+  const startCell = document.querySelectorAll(`td`)[combination[0]];
+  const endCell = document.querySelectorAll(`td`)[combination[2]];
+  const startRect = startCell.getBoundingClientRect();
+  const endRect = endCell.getBoundingClientRect();
+
+  const contentRect = document.getElementById('content').getBoundingClientRect();
+
+  const lineLength = Math.sqrt(
+    Math.pow(endRect.left - startRect.left, 2) + Math.pow(endRect.top - startRect.top, 2)
+  );
+  const lineAngle = Math.atan2(endRect.top - startRect.top, endRect.left - startRect.left);
+
+  const line = document.createElement('div');
+  line.style.position = 'absolute';
+  line.style.width = `${lineLength}px`;
+  line.style.height = `${lineWidth}px`;
+  line.style.backgroundColor = lineColor;
+  line.style.top = `${startRect.top + startRect.height / 2 - lineWidth / 2 - contentRect.top}px`;
+  line.style.left = `${startRect.left + startRect.width / 2 - contentRect.left}px`;
+  line.style.transform = `rotate(${lineAngle}rad)`;
+  line.style.transformOrigin = `top left`;
+  document.getElementById('content').appendChild(line);
 }
